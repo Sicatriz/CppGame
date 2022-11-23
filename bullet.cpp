b1e1d0fd0f869d8106c1a5f9c2ad037aa2a6468c
Miguel <miguel.nunez@student.pxl.be>
2022-11-23 14:59:40 +0100

    Base Code
    Samen met Indy opgesteld

---
+++ bullet.cpp
@@ -0,0 +1,22 @@
+#include "bullet.h"
+#include "myrect.h"
+#include <QTimer>
+
+Bullet::Bullet()
+{
+    // draw the rect
+    setRect(0,0,10,50);
+
+    // connect
+    QTimer * timer = new QTimer();
+    connect(timer,SIGNAL(timeout()),this,SLOT(move()));
+
+    timer->start(50);
+
+}
+
+void Bullet::move()
+{
+    // move bullet up
+    setPos(x(),y()-10);
+}
