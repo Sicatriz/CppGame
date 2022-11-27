b42dd2c5b09869f0b041ed8fdb6515e7113112a1
Miguel <miguel.nunez@student.pxl.be>
2022-11-27 12:07:19 +0100

    correction virtual

--- bullet.h
+++ bullet.h
@@ -9,7 +9,7 @@ class Bullet: public QObject, public QGraphicsRectItem
 {
    Q_OBJECT    // bullet needs to be an Qobject for movement and this makes it a object.
 public:
-    Bullet(QGraphicsItem * parent=0);
+    Bullet(QGraphicsItem * parent=0);  /*** argumented constructor ****/
 public slots:  //slots let's you connect signals to your function.
     void move();
 
